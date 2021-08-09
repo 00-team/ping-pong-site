ee155dae60a18d06f0078109f9dd618945109d4a
sadra <sadrataghavi1383@gmail.com>
2021-08-09 22:34:22 +0430

    setting full page for slider

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// components 
+import Slider from '../Slider/Slider'
+
+// css
+import "./HeroSection.css"
+
+
+function HeroSection() {
+    return (
+        <div className="heroSection">
+            <Slider/>
+        </div>
+    )
+}
+
+export default HeroSection
