632df38497686257a821a68ce57f43fe4fb3fad1
i007c <dr007cc@gmail.com>
2021-08-10 17:32:52 +0430

    add a message

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 // components
 // import HeroSection from '../Components/HeroSection'
+// for image slider use this lib
+// https://swiperjs.com/demos
+// its so better and pls clean up your code! <3
 
 function Home() {
     return (
