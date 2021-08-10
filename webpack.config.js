11d276df9a80740e2060640b13ef3c0d9e10c882
i007c <dr007cc@gmail.com>
2021-08-10 16:27:24 +0430

    restore some of files

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        sourceMapFilename: 'SourceMaps/[file].map'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: [/\.s[ac]ss$/i, /\.css$/i],
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/template.html',
+            inject: true,
+            favicon: './src/img/favicon.ico',
+            publicPath: '/',
+            templateParameters: {
+                SiteName: 'Ping Pong'
+            }
+        }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+};
