955ad90c25655a75601758cbcb30b63aa1333fdd
sadra <sadrataghavi1383@gmail.com>
2021-08-14 20:23:02 +0430

    clean cod

--- src/components/layouts/Glories.js
+++ src/components/layouts/Glories.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Glories(props) {
+const Glories = ({label,src,text}) => {
   return (
     <>
-      <li className='cards__item'>
-        <div className='cards__item__link'>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
-              alt='Travel Image'
-              src={props.src}
-            />
-          </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
-          </div>
-        </div>
-      </li>
+        <li className='cards__item'>
+            <div className='cards__item__link'>
+                <figure className='cards__item__pic-wrap' data-category={label}>
+                    <img
+                      className='cards__item__img'
+                      alt='Travel Image'
+                      src={src}
+                    />
+                </figure>
+                <div className='cards__item__info'>
+                    <h5 className='cards__item__text'>{text}</h5>
+                </div>
+            </div>
+        </li>
     </>
   );
 }
