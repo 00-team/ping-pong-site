d83cd80bdadd57617e023867b98cff1937587267
i007c <dr007cc@gmail.com>
2021-08-15 23:40:02 +0430

    handel file-loader bugs and add other stuffs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+var config = {
     entry: './src/App.js',
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        sourceMapFilename: 'SourceMaps/[file].map'
+        sourceMapFilename: 'SourceMaps/[file].map',
+        assetModuleFilename: 'static/[hash][ext][query]',
     },
     module: {
         rules: [
@@ -15,22 +16,26 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
-                }
+                    loader: 'babel-loader',
+                },
             },
             {
                 test: [/\.s[ac]ss$/i, /\.css$/i],
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                test: /\.(ico)$/i,
                 use: [
                     {
                         loader: 'file-loader',
+                        options: {
+                            outputPath: 'static',
+                        },
                     },
-                ]
+                ],
+                type: 'javascript/auto',
             },
-        ]
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -40,12 +45,20 @@ module.exports = {
             favicon: './src/static/img/favicon.ico',
             publicPath: '/',
             templateParameters: {
-                SiteName: 'Ping Pong'
-            }
+                SiteName: 'Ping Pong',
+            },
         }),
     ],
-    devtool: 'source-map',
     devServer: {
-        contentBase: './dist',
+        compress: true,
+        port: 8000,
     },
-};
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map'
+    }
+
+    return config
+}
