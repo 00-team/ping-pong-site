5aa4a3797a2566aaeb746c42acf1f387e37b5305
i007c <dr007cc@gmail.com>
2021-08-16 04:27:41 +0430

    clean code

--- src/database/reducers/locale/locale.js
+++ src/database/reducers/locale/locale.js
@@ -7,20 +7,20 @@ const AllowdLocales = data.map(d => d.locale)
 
 const initState = {
     locale: AllowdLocales[0] || null,
-    localeData: data[0]
+    localeData: data[0],
 }
 
-export default (state=initState, action) => {
+export default (state = initState, action) => {
     switch (action.type) {
         case CHANGE_LOCALE:
             if (AllowdLocales.includes(action.payload)) {
                 return {
                     ...state,
                     locale: action.payload,
-                    localeData: data.find(l => l.locale === action.payload)
+                    localeData: data.find(l => l.locale === action.payload),
                 }
             } else return state
-            
+
         default:
             return state
     }
