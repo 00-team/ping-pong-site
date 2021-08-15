eed5eab26448df610de4601d44c2662607f5c078
i007c <dr007cc@gmail.com>
2021-08-16 04:25:16 +0430

    clean code

--- src/components/layouts/Intro.js
+++ src/components/layouts/Intro.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 // redux
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 // commons
 import Button from '../common/Button'
@@ -10,7 +10,6 @@ import Button from '../common/Button'
 import './sass/intro.scss'
 
 const Intro = () => {
-    const dispatch = useDispatch()
     const Locale = useSelector(state => state.Locale.localeData)
     const [introData, setIntroData] = useState({
         name: '',
@@ -22,21 +21,12 @@ const Intro = () => {
         if (Locale) setIntroData(Locale.text.intro)
     }, [Locale])
 
-    const ToggleLocale = () => {
-        dispatch({
-            type: 'CHANGE_LOCALE',
-            payload: Locale.locale === 'en' ? 'fa' : 'en',
-        })
-    }
-
     return (
         <div className='intro-container'>
-            <div className='intro' style={{ direction: Locale.direction }} >
+            <div className='intro' style={{ direction: Locale.direction }}>
                 <span className='name'>{introData.name}</span>
                 <span className='job'>{introData.job}</span>
-                <Button onClick={e => ToggleLocale()}>
-                    {introData.button}
-                </Button>
+                <Button onClick={e => {}}>{introData.button}</Button>
             </div>
         </div>
     )
