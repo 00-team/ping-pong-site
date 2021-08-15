0cb55308725e95f8235021ea284c54b56519d98e
i007c <dr007cc@gmail.com>
2021-08-15 23:54:02 +0430

    handel locale style

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 // redux
-import { Provider as ReduxProvider, useDispatch, useSelector } from 'react-redux';
-import store from './database/store';
+import {
+    Provider as ReduxProvider,
+    useDispatch,
+    useSelector,
+} from 'react-redux'
+import store from './database/store'
 
 // components
-
+import Intro from './components/layouts/Intro'
 
 // style
 import './components/sass/base.scss'
 
 const App = () => {
-    const dispatch = useDispatch()
-    const currentLocale = useSelector(state => state.Locale.locale)
-    
-    console.log(currentLocale);
+    const Locale = useSelector(state => state.Locale)
 
-    
+    useEffect(() => {
+        if (Locale)
+            if (Locale.localeData) {
+                document.body.className = Locale.localeData.direction
+            }
+    }, [Locale])
+
     return (
-        <>App</>
+        <>
+            <Intro />
+        </>
     )
 }
 
 export default App
 
-
 const Root = () => {
     return (
         <ReduxProvider store={store}>
-            <App/>
+            <App />
         </ReduxProvider>
     )
 }
 
-ReactDOM.render(<Root/>, document.getElementById('root'))
+ReactDOM.render(<Root />, document.getElementById('root'))
