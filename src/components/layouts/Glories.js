0f57ff8b29ae87b2ee90477c7807d9a1dbde902d
sadra <sadrataghavi1383@gmail.com>
2021-08-17 14:01:19 +0430

    clean code

--- src/components/layouts/Glories.js
+++ src/components/layouts/Glories.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Glories = ({label,src,text}) => {
-  return (
-    <>
-        <li className='cards__item'>
-            <div className='cards__item__link'>
-                <figure className='cards__item__pic-wrap' data-category={label}>
-                    <img
-                      className='cards__item__img'
-                      alt='Travel Image'
-                      src={src}
-                    />
-                </figure>
-                <div className='cards__item__info'>
-                    <h5 className='cards__item__text'>{text}</h5>
+const Glories = ({ label, src, text }) => {
+    return (
+        <>
+            <li className='cards__item'>
+                <div className='cards__item__link'>
+                    <figure
+                        className='cards__item__pic-wrap'
+                        data-category={label}
+                    >
+                        <img
+                            className='cards__item__img'
+                            alt='Travel Image'
+                            src={src}
+                        />
+                    </figure>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text fa'>{text}</h5>
+                    </div>
                 </div>
-            </div>
-        </li>
-    </>
-  );
+            </li>
+        </>
+    )
 }
 
-export default Glories;
+export default Glories
