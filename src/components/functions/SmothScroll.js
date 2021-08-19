354213a0c722ff0222efcfc88bc54bfb60bb2807
i007c <dr007cc@gmail.com>
2021-08-19 10:03:13 +0430

    remove log line

--- src/components/functions/SmothScroll.js
+++ src/components/functions/SmothScroll.js
@@ -5,7 +5,6 @@ export default e => {
     if (e.offsetTop < window.scrollY) direction = -7
 
     const anime = () => {
-        console.log(direction, window.scrollY)
         if (direction === +7 && window.scrollY >= e.offsetTop) {
             window.scroll(0, e.offsetTop)
             return
