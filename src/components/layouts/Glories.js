0d621ab310971b83dc67c4c7cf7c503f49f1d9b8
i007c <dr007cc@gmail.com>
2021-08-19 10:00:40 +0430

    init glories

---
+++ src/components/layouts/Glories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// style
+import './sass/glories.scss'
+
+const Glories = () => {
+    return (
+        <div className='glories-container' id='glories' >
+            <div className='glories'></div>
+        </div>
+    )
+}
+
+export default Glories
