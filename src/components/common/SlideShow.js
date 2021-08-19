bd18c652f9700f7704fc1ad2645cb86ad2052a42
i007c <dr007cc@gmail.com>
2021-08-20 01:05:24 +0430

    make slider component

---
+++ src/components/common/SlideShow.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+// style
+import './sass/slide-show.scss'
+
+const qs = q => document.querySelector(q)
+
+const SlideShow = ({ children, NextIcon, PreviousIcon }) => {
+    const [slides, setSlides] = useState({ length: 0, index: 0 })
+    const slidesContainer = useRef(null)
+
+    useEffect(() => {
+        if (children) setSlides({ ...slides, length: children.length })
+    }, [children])
+
+    useEffect(() => {
+        window.onresize = () => {
+            let sindex = qs('.slides-container').getAttribute('data-index')
+            let ow = qs('.slide').offsetWidth
+            qs('.slides-container').style.transform = `translateX(-${
+                ow * sindex
+            }px)`
+        }
+        return () => {
+            window.onresize = null
+        }
+    }, [])
+
+    const MoveSlide = direction => {
+        let action
+        if (direction === 'next') {
+            if (slides.index >= slides.length - 1) return
+            action = +1
+        } else if (direction === 'previous') {
+            if (slides.index === 0) return
+            action = -1
+        } else return
+
+        let ow = qs('.slide').offsetWidth
+
+        if (slidesContainer.current) {
+            slidesContainer.current.style.transform = `translateX(-${
+                ow * (slides.index + action)
+            }px)`
+        }
+
+        setSlides({ ...slides, index: slides.index + action })
+    }
+
+    return (
+        <div className='slider-container'>
+            <div
+                className={
+                    'icon-container previous-icon' +
+                    (slides.index === 0 ? ' disable' : '')
+                }
+                onClick={e => MoveSlide('previous')}
+            >
+                {PreviousIcon}
+            </div>
+            <div className='sliders-container'>
+                <div className='slides-container' ref={slidesContainer} data-index={slides.index}>
+                    {children &&
+                        children.map((slide, index) => (
+                            <div key={index} className='slide'>
+                                {slide}
+                            </div>
+                        ))}
+                </div>
+            </div>
+            <div
+                className={
+                    'icon-container next-icon' +
+                    (slides.index >= slides.length - 1 ? ' disable' : '')
+                }
+                onClick={e => MoveSlide('next')}
+            >
+                {NextIcon}
+            </div>
+        </div>
+    )
+}
+
+SlideShow.defaultProps = {
+    children: '',
+    NextIcon: null,
+    PreviousIcon: null,
+}
+
+export default SlideShow
