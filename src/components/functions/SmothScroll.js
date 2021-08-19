d6bdda3f1d640ea9a9540a5570bf1e23487b4038
i007c <dr007cc@gmail.com>
2021-08-19 10:00:17 +0430

    debuging

--- src/components/functions/SmothScroll.js
+++ src/components/functions/SmothScroll.js
@@ -1,9 +1,20 @@
 export default e => {
-    if (!e) return;
+    if (!e) return
+    let direction = +7
+
+    if (e.offsetTop < window.scrollY) direction = -7
 
     const anime = () => {
-        if (window.scrollY >= e.offsetTop) return;
-        window.scroll(0, window.scrollY + 7)
+        console.log(direction, window.scrollY)
+        if (direction === +7 && window.scrollY >= e.offsetTop) {
+            window.scroll(0, e.offsetTop)
+            return
+        } else if (direction === -7 && window.scrollY <= e.offsetTop) {
+            window.scroll(0, e.offsetTop)
+            return
+        }
+
+        window.scroll(0, window.scrollY + direction)
         setTimeout(anime, 1)
     }
     anime()
