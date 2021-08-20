6acdf1fa9a2a6960fa6be61ca0b39f52862fdc52
i007c <dr007cc@gmail.com>
2021-08-20 10:41:36 +0430

    add smaple video for students

--- src/components/layouts/Students.js
+++ src/components/layouts/Students.js
@@ -1,59 +1,88 @@
 import React, { useState, useEffect, useRef } from 'react'
 
+// redux
+import { useSelector } from 'react-redux'
+
+// icons
+import { CgClose } from 'react-icons/cg'
+
 // functions
 import AutoScroll from '../functions/AutoScroll'
+
+// common
+import Button from '../common/Button'
 
 // style
 import './sass/students.scss'
 
+// files
+import videoFile from '../../static/videos/sample.mp4'
+
 const Students = () => {
     const studentContainer = useRef(null)
     const [scrolling, setScrolling] = useState(true)
+    const [showVideo, setShowVideo] = useState(true)
 
-    const stodents = [
+    const [studentInitList, setSIL] = useState([
         {
             id: 1,
-            name: 'Student 1',
-            description: 'a random dude in asia, win wrold champ xD',
-            img: 'https://media.istockphoto.com/photos/asian-guy-playing-table-tennis-picture-id889418740?s=612x612',
+            img: 'https://cdn.discordapp.com/attachments/731174051170746500/836798695139573840/007_logo_f27.png',
+            year: 2019,
         },
         {
             id: 2,
-            name: 'Student 2',
-            description: 'a random dude in asia, win wrold champ xD',
-            img: 'https://img.freepik.com/free-photo/confident-young-pretty-sporty-girl-wearing-headband-wristband-holding-ping-pong-rackets-isolated-blue-wall-with-copy-space_141793-101372.jpg?size=626&ext=jpg&ga=GA1.2.357280364.1626393600',
+            img: 'https://cdn.discordapp.com/attachments/731174051170746500/808355617441710130/mars-planet-vector-13949696.jpg',
+            year: 2021,
         },
-        {
-            id: 3,
-            name: 'Student 3',
-            description:
-                'a random dude in asia, win wrold champ xD a random dude in asia, win wrold champ xD a random dude in asia, win wrold champ xD a random dude in asia, win wrold champ xD',
-            img: 'https://previews.123rf.com/images/zoomteam/zoomteam1203/zoomteam120300278/12763547-sexy-sports-girl-plays-table-tennis.jpg',
-        },
-    ]
+    ])
 
-    const stodents2 = [
-        {
-            id: 1,
-            name: 'Student 1',
-            description: 'a random dude in asia, win wrold champ xD',
-            img: 'https://media.istockphoto.com/photos/asian-guy-playing-table-tennis-picture-id889418740?s=612x612',
-        },
-        {
-            id: 2,
-            name: 'Student 2',
-            description: 'a random dude in asia, win wrold champ xD',
-            img: 'https://img.freepik.com/free-photo/confident-young-pretty-sporty-girl-wearing-headband-wristband-holding-ping-pong-rackets-isolated-blue-wall-with-copy-space_141793-101372.jpg?size=626&ext=jpg&ga=GA1.2.357280364.1626393600',
-        },
-        {
-            id: 3,
-            name: 'Student 3',
-            description:
-                'a random dude in asia, win wrold champ xD a random dude in asia, win wrold champ xD a random dude in asia, win wrold champ xD a random dude in asia, win wrold champ xD',
-            img: 'https://previews.123rf.com/images/zoomteam/zoomteam1203/zoomteam120300278/12763547-sexy-sports-girl-plays-table-tennis.jpg',
-        },
-    ]
+    const [studentList, setStudentList] = useState({
+        list1: [],
+        list2: [],
+    })
 
+    const Locale = useSelector(state => state.Locale.localeData)
+    const [studentsData, setStudentData] = useState({
+        title: '',
+        button: '',
+    })
+
+    // locale + student data
+    useEffect(() => {
+        if (Locale) setStudentData(Locale.text.students)
+        if (
+            typeof Locale.text.students.studentList === 'object' &&
+            Locale.text.students.studentList
+        ) {
+            let Stud = []
+            Locale.text.students.studentList.forEach(s => {
+                let sf = studentInitList.find(i => i.id === s.id)
+                if (sf) Stud.push({ ...sf, ...s })
+            })
+            setSIL(Stud)
+        }
+    }, [Locale])
+
+    // student data
+    useEffect(() => {
+        let list1 = []
+        let list2 = []
+
+        if (studentInitList.length)
+            if (studentInitList[0].name) {
+                studentInitList.forEach((sil, index) => {
+                    if (index % 2 === 0) {
+                        list1.push(sil)
+                    } else {
+                        list2.push(sil)
+                    }
+                })
+
+                setStudentList({ list1: list1, list2: list2 })
+            }
+    }, [studentInitList])
+
+    // scroll animation
     useEffect(() => {
         let autoScrollInterval
         if (studentContainer.current && scrolling) {
@@ -69,7 +98,9 @@ const Students = () => {
     return (
         <div className='students-container' id='students'>
             <div className='students'>
-                <h2>Students</h2>
+                <h2 style={{ direction: Locale.direction }}>
+                    {studentsData.title}
+                </h2>
                 <div
                     className='student-container'
                     ref={studentContainer}
@@ -77,41 +108,59 @@ const Students = () => {
                     onMouseLeave={e => setScrolling(true)}
                 >
                     <div className='col'>
-                        {stodents.map((s, i) => (
+                        {studentList.list1.map((s, i) => (
                             <div key={i} className='student'>
                                 <div
                                     className='pic'
                                     style={{ backgroundImage: `url(${s.img})` }}
                                 ></div>
-                                <div className='bio'>
+                                <div
+                                    className='bio'
+                                    style={{ direction: Locale.direction }}
+                                >
                                     <span className='name'>{s.name}</span>
                                     <span className='description'>
                                         {s.description}
                                     </span>
-                                    <span className='year'>2019</span>
+                                    <span className='year'>{s.year}</span>
                                 </div>
                             </div>
                         ))}
                     </div>
                     <div className='col'>
-                        {stodents2.map((s, i) => (
+                        {studentList.list2.map((s, i) => (
                             <div key={i} className='student'>
                                 <div
                                     className='pic'
                                     style={{ backgroundImage: `url(${s.img})` }}
                                 ></div>
-                                <div className='bio'>
+                                <div
+                                    className='bio'
+                                    style={{ direction: Locale.direction }}
+                                >
                                     <span className='name'>{s.name}</span>
                                     <span className='description'>
                                         {s.description}
                                     </span>
-                                    <span className='year'>2210</span>
+                                    <span className='year'>{s.year}</span>
                                 </div>
                             </div>
                         ))}
                     </div>
                 </div>
+                <Button onClick={e => setShowVideo(true)}>{studentsData.button}</Button>
             </div>
+
+            {showVideo && (
+                <div className='video-container'>
+                    <div className='close' onClick={e => setShowVideo(false)}>
+                        <CgClose />
+                    </div>
+                    <div className='video'>
+                        <video src={videoFile} controls></video>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
