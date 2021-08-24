5c64ff44e57f2ea9eed30ff01db140dda7b20317
sadra <sadrataghavi1383@gmail.com>
2021-08-24 21:27:51 +0430

    gonna finish students tommorow

--- src/components/layouts/Students.js
+++ src/components/layouts/Students.js
@@ -3,9 +3,37 @@ import React, { useEffect, useState } from 'react'
 import { SliderData } from '../elements/SliderData'
 import StudentSlider from '../elements/studentSlider'
 
+// Swiper 
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+    Autoplay, Pagination, Navigation, EffectCoverflow
+} from 'swiper/core';
+
+SwiperCore.use([Autoplay, Pagination, Navigation, EffectCoverflow]);
+
+
+// style 
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
+import "../css/imgSlider.css"
+
 // css
 import "./sass/students.scss"
 
+
+//imgs
+import radin        from "../../static/students/radin.jpg"
+import nilia        from '../../static/students/nilia.jpg'
+import shaghayegh   from '../../static/students/shaghayegh.jpg'
+
+const images = [
+    { img: radin,      discription: ' رادین خیام قهرمان کشور و دارنده رنکینگ جهانی', id: 1 },
+    { img: nilia,      discription: 'نیلیا نقیبی دارنده مقام اول در مسابقات کشوری', id: 2 },
+    { img: shaghayegh, discription: 'شقایث شهریاری نایب قهرمان لیگ بزرگسالان', id: 3 },
+]
+
+//
 const Students = (props) => {
 
 
@@ -19,7 +47,7 @@ const Students = (props) => {
                 شاگردان محمود ریزبند
             </h1>
             <div className='students_vids container'>
-                {/* <StudentSlider slides={SliderData} /> */}
+                
             </div>
         </div>
     )
