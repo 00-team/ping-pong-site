1c6385ab1cfce1530aaed54d302254e387dc1623
sadra <sadrataghavi1383@gmail.com>
2021-08-28 17:07:09 +0430

    edited 007 github icon link

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -11,40 +11,40 @@ import "../css/footer.css";
 
 function Footer() {
     return (
-      <div>
-        <div className="footer-container" id="contact_us">
-          <section className="social-media">
-            <div className="social-media-wrap">
-              <div className="footer-logo">
-                <Link to="/" className="social-logo">
-                  Created By : 00 Team
-                  <i className="fab fa-typo3" />
-                </Link>
-              </div>
-              <small className="website-rights">© 2021</small>
-              <div className="team" id="team">
-                <Link
-                  className="social-icon-link facebook"
-                  to="/"
-                  target="_blank"
-                  aria-label="Facebook"
-                >
-                  <FaGithubSquare />
-                </Link>
-                <Link
-                  className="social-icon-link instagram"
-                  to="/"
-                  target="_blank"
-                  aria-label="Instagram"
-                >
-                  <i className="fab fa-instagram" />
-                </Link>
-              </div>
+        <div>
+            <div className='footer-container' id='contact_us'>
+                <section className='social-media'>
+                    <div className='social-media-wrap'>
+                        <div className='footer-logo'>
+                            <Link to='/' className='social-logo'>
+                                Created By : 00 Team
+                                <i className='fab fa-typo3' />
+                            </Link>
+                        </div>
+                        <small className='website-rights'>© 2021</small>
+                        <div className='team' id='team'>
+                            <a
+                                className='social-icon-link facebook'
+                                href='https://github.com/00-team'
+                                target='_blank'
+                                aria-label=''
+                            >
+                                <FaGithubSquare />
+                            </a>
+                            <Link
+                                className='social-icon-link instagram'
+                                to='/'
+                                target='_blank'
+                                aria-label='Instagram'
+                            >
+                                <i className='fab fa-instagram' />
+                            </Link>
+                        </div>
+                    </div>
+                </section>
             </div>
-          </section>
         </div>
-      </div>
-    );
+    )
 }
 
 export default Footer
